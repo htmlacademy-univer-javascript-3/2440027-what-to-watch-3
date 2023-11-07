1619726eb019467bb1b2eaf3524f0d7a9c585088
perceptr <st.arms.all@gmail.com>
2023-11-07 23:38:40 +0500

    preview player ready

--- src/components/movie-card/movie-card.tsx
+++ src/components/movie-card/movie-card.tsx
@@ -1,5 +1,8 @@
 /* eslint-disable react/no-unused-prop-types */
+import React from 'react';
 import { Link } from 'react-router-dom';
+import VideoPlayer from '../utils/video-player';
+import useDelayedHover from '../../hooks/use-delayed-hover';
 
 type MovieCardProps = {
   id: string;
@@ -8,26 +11,35 @@ type MovieCardProps = {
   description?: string;
   releaseDate?: string;
   genre?: string;
+  trailerSrc: string;
   onMouseEnter?: () => void;
   onMouseLeave?: () => void;
 };
 
-function MovieCard(props: MovieCardProps) {
+const MovieCard: React.FC<MovieCardProps> = ({ id, title, imageSrc, trailerSrc }) => {
+  const { isPlaying, onMouseEnter, onMouseLeave } = useDelayedHover(1000);
+
   return (
     <article
       className="small-film-card catalog__films-card"
-      onMouseEnter={props.onMouseEnter}
-      onMouseLeave={props.onMouseLeave}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
     >
       <div className="small-film-card__image">
-        <img src={props.imageSrc} alt={props.title} width="280" height="175" />
+        {isPlaying ? (
+          <VideoPlayer src={trailerSrc} poster={imageSrc} isPlaying={isPlaying} />
+        ) : (
+          <img src={imageSrc} alt={title} width="280" height="175" />
+        )}
       </div>
       <h3 className="small-film-card__title">
-        <Link to={`/films/${props.id}`} className="small-film-card__link">{props.title || ''}
+        <Link to={`/films/${id}`} className="small-film-card__link">
+          {title}
         </Link>
       </h3>
     </article>
   );
-}
+};
 
 export default MovieCard;
+
---
+++ src/hooks/use-delayed-hover.ts
@@ -0,0 +1,33 @@
+import { useState, useEffect, useRef } from 'react';
+
+const useDelayedHover = (delay: number) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const timeoutIdRef = useRef<NodeJS.Timeout | null>(null);
+
+  useEffect(() => {
+    if (isHovered) {
+      timeoutIdRef.current = setTimeout(() => setIsPlaying(true), delay);
+    } else {
+      if (timeoutIdRef.current) {
+        clearTimeout(timeoutIdRef.current);
+        timeoutIdRef.current = null;
+      }
+      setIsPlaying(false);
+    }
+
+    return () => {
+      if (timeoutIdRef.current) {
+        clearTimeout(timeoutIdRef.current);
+      }
+    };
+  }, [isHovered, delay]);
+
+  return {
+    isPlaying,
+    onMouseEnter: () => setIsHovered(true),
+    onMouseLeave: () => setIsHovered(false),
+  };
+};
+
+export default useDelayedHover;
